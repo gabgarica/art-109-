67e72a3be66862ae67e83f19834fb201fa29c272
gabgarica <hbkcassie@gmail.com>
2025-09-26 09:54:30 -0700

    tut DOM

--- class-exercises/dom/js/main.js
+++ class-exercises/dom/js/main.js
@@ -1,3 +1,10 @@
 
 console.log("hello");
 
+//Javascript Timeout changes h1 title after 3 seconds//
+setTimeout(function(){
+    document.querySelector("#page-title").style.color = "pink" 
+    console.log("timeout worked!");
+} ,3000)
+
+
