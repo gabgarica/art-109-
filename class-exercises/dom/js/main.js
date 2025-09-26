f01a95d217e5792c75a5815d8668f3fd2940677c
gabgarica <hbkcassie@gmail.com>
2025-09-26 11:14:27 -0700

    dom tut 5
    switching to appearing and disappearing images

--- class-exercises/dom/js/main.js
+++ class-exercises/dom/js/main.js
@@ -1,10 +1,23 @@
 
 console.log("hello");
 
+let pageTitle = document.querySelector("#page-title");
+
+let header = document.querySelector("header"); 
+
+let body = document.querySelector("body");
+
 //Javascript Timeout changes h1 title after 3 seconds//
 setTimeout(function(){
-    document.querySelector("#page-title").style.color = "pink" 
-    console.log("timeout worked!");
+    pageTitle.style.color = "pink" 
+    //console.log("timeout worked!");
 } ,3000)
+
+//click event on header changes background color
+
+header.onclick = function() {
+//console.log("clicked header");
+body.style.backgroundColor = "black";
+}
 
 
