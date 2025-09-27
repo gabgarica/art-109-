8fd18e519a67382fe5270f75a69601b14799f49b
gabgarica <hbkcassie@gmail.com>
2025-09-26 20:30:29 -0700

    tut 5
    completed tut 5

--- class-exercises/dom/js/main.js
+++ class-exercises/dom/js/main.js
@@ -1,5 +1,39 @@
 
-console.log("hello");
+document.querySelector("#image-0").addEventListener("click", function (){
+    document.querySelector("#image-1").style.visibility = "visible";
+    alert("bark bark bark");
+})
+document.querySelector("#image-1").addEventListener("click", function (){
+    document.querySelector("#image-2").style.visibility = "visible";
+})
+document.querySelector("#image-2").addEventListener("click", function (){
+    document.querySelector("#image-3").style.visibility = "visible";
+})
+document.querySelector("#image-3").addEventListener("click", function (){
+    document.querySelector("#image-4").style.visibility = "hidden";
+})
+document.querySelector("#image-4").addEventListener("click", function (){
+    document.querySelector("#image-5").style.visibility = "visible";
+})
+document.querySelector("#image-5").addEventListener("click", function (){
+    document.querySelector("#image-6").style.visibility = "visible";
+})
+document.querySelector("#image-6").addEventListener("click", function (){
+   // document.querySelector("#image-6").style.visibility = "hidden";
+})
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log("hello");
 
 let pageTitle = document.querySelector("#page-title");
 
@@ -19,5 +53,5 @@ header.onclick = function() {
 //console.log("clicked header");
 body.style.backgroundColor = "black";
 }
-
+*/
 
