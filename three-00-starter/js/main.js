67b310e62f3a8166e990494de13582e1c4436996
gabgarica <hbkcassie@gmail.com>
2025-11-07 20:26:13 -0800

    tut

--- three-00-starter/js/main.js
+++ three-00-starter/js/main.js
@@ -2,6 +2,7 @@
 // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
 
 
+
 //~~~~~~~Import Three.js (also linked to as import map in HTML)~~~~~~
 import * as THREE from 'three';
 
@@ -11,37 +12,68 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 
 // ~~~~~~~~~~~~~~~~Set up~~~~~~~~~~~~~~~~
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer();
+let scene, camera, renderer, cube;
+
+function init() {
+
+    const light = new THREE.DirectionalLight(0xffffff, 3 );
+    light.position.set(1,1,5);
+    scene.add(light);
+
+ scene = new THREE.Scene();
+ camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+ renderer = new THREE.WebGLRenderer({antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(2,2,2);
-const material = THREE.MeshBasicMaterial(  {color:0x0000ff} );
-const cube = THREE.Mesh( geometry,material);
-scene.add( cube);
+const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+//const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+
+//const geometry = new THREE.CapsuleGeometry( 1, 1, 4, 8, 1 );
+
+//const texture = new THREE.TextureLoader().load('textures/check.jpg');
+//const material = new THREE.MeshBasicMaterial( { map: texture } );
+
+//cube = new THREE.Mesh( geometry, material );
+//scene.add( cube );
 
 camera.position.z = 5;
-
+ 
+}
 function animate() {
     requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01
+    //cube.rotation.x += 0.01;
+    //cube.rotation.y += 0.01
 
     renderer.render(scene, camera);
 }
 
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+}
+
+window.addEventListener('resize', onWindowResize, false);
+
+init();
 animate();
 
 
 // ~~~~~~~~~~~~~~~~ Initiate add-ons ~~~~~~~~~~~~~~~~
 const controls = new OrbitControls(camera, renderer.domElement);
-// const loader = new GLTFLoader(); // to load 3d models
+const loader = new GLTFLoader(); // to load 3d models
 
+loader.load('assets/dog_shiny.gltf', function (gltf) {
+    const dog = gltf.scene;
+    scene.add(dog);
+    dog.scale.set(2,2,2);
 
+});
 
 // ~~~~~~~~~~~~~~~~ Create scene here ~~~~~~~~~~~~~~~~
 // →→→→→→ Follow next steps in tutorial: 
