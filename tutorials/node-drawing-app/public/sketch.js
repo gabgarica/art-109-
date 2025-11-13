6e50e1814751e8d1269c8aa06e9baa7347f52410
gabgarica <hbkcassie@gmail.com>
2025-11-12 21:43:04 -0800

    3rd time
    im so lost

--- tutorials/node-drawing-app/public/sketch.js
+++ tutorials/node-drawing-app/public/sketch.js
@@ -1,9 +1,16 @@
+var socket; 
+
 function setup() {
-createCanvas(200,200);
+createCanvas(600,400);
+background(51);
+
+socket = socket.io.connect('http://localhost:3000')
 }
 
 function draw() {
-    background(51);
-    ellipse(mouseX,mouseY, 60, 60);
+    
+    noStroke();
+    fill(255);
+    ellipse(mouseX,mouseY, 36, 36);
  
 }
