265261dfb35846b93ad092399ad92f7e7bfe8f2b
gabgarica <hbkcassie@gmail.com>
2025-11-14 00:32:51 -0800

    heh

--- tutorials/node-drawing-app/public/sketch.js
+++ tutorials/node-drawing-app/public/sketch.js
@@ -4,13 +4,32 @@ function setup() {
 createCanvas(600,400);
 background(51);
 
-socket = socket.io.connect('http://localhost:3000')
+socket = io.connect('http://localhost:3000')
+socket.on('mouse', newDrawing);
+}
+
+function newDrawing(data) {
+    noStroke();
+    fill(255, 0, 100);
+    ellipse(data.x, data.y, 36, 36);
+}
+
+function mouseDragged() {
+    console.log('Sending: ' + mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX, 
+        y: mouseY
+    }
+
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, 36, 36);
 }
 
 function draw() {
     
-    noStroke();
-    fill(255);
-    ellipse(mouseX,mouseY, 36, 36);
  
 }
